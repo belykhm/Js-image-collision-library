bcf822511a35a66361d23d3d2ff434856f3dda58
Michael Belykh <belykhm@yahoo.ca>
2019-05-30 19:51:15 -0600

    Version 1.0

---
+++ Collision_Library.js
@@ -0,0 +1,38 @@
+﻿
+
+function crash(id1, id2) {
+
+    var TopPositionElement1 = document.getElementById(id1).offsetTop;
+    var LeftPositionElement1 = document.getElementById(id1).offsetLeft;
+    var widthElement1 = document.getElementById(id1).offsetWidth;
+    var heightElement1 = document.getElementById(id1).offsetHeight;  
+    var TopPositionElement2 = document.getElementById(id2).offsetTop;
+    var LeftPositionElement2 = document.getElementById(id2).offsetLeft;
+    var widthElement2 = document.getElementById(id2).offsetWidth;
+    var heightElement2 = document.getElementById(id2).offsetHeight;
+    var BottomElement1 = TopPositionElement1 + heightElement1;
+    var rightElement1 = LeftPositionElement1 + widthElement1;
+    var BottomElement2 = TopPositionElement2 + heightElement2;
+    var rightElement2 = LeftPositionElement2 + widthElement2;
+    //debugger;
+
+    if (LeftPositionElement1 > rightElement2 || LeftPositionElement2 > rightElement1) {
+        return false;
+    }
+
+    if (TopPositionElement1 > BottomElement2 || TopPositionElement2 > BottomElement1) {
+        return false;
+    }
+
+    console.log("LeftPositionElement1:" + LeftPositionElement1);
+    console.log("LeftPositionElement2:" + LeftPositionElement2);
+    console.log("rightElement1:" + rightElement1);
+    console.log("rightElement2:" + rightElement2);
+
+    return true;
+}
+
+// To call:
+//if (crash("ball 1 id", "ball 2 id")) {
+    // do sometthing
+//}
